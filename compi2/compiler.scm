3aa61dd143cf057dbd2d6506ee853bd92bc8ad73
guy7145 <guy7145@gmail.com>
2016-12-11 16:08:21 +0200

    moved if's to guy-rules.scm

--- compi2/compiler.scm
+++ compi2/compiler.scm
@@ -714,18 +714,6 @@
    (? 'var var?)
    (lambda (var) `(var ,var))))
 
-(define <if2-rule>
-  (pattern-rule
-   `(if ,(? 'test) ,(? 'dit))
-   (lambda (test dit)
-     `(if3 ,(run test) ,(run dit) `(const ,*void-object*)))))
-
-(define <if3-rule>
-  (pattern-rule
-   `(if ,(? 'test) ,(? 'dit) ,(? 'dif))
-   (lambda (test dit dif)
-     `(if3 ,(run test) ,(run dit) ,(run dif)))))
-
 (load "guy-rules.scm")
 (load "max-rules.scm")
 
---
+++ compi2/guy-rules.scm
@@ -0,0 +1,12 @@
+(define <if2-rule>
+  (pattern-rule
+   `(if ,(? 'test) ,(? 'dit))
+   (lambda (test dit)
+     `(if3 ,(run test) ,(run dit) `(const ,*void-object*)))))
+
+(define <if3-rule>
+  (pattern-rule
+   `(if ,(? 'test) ,(? 'dit) ,(? 'dif))
+   (lambda (test dit dif)
+     `(if3 ,(run test) ,(run dit) ,(run dif)))))
+
