3e0e3baff76e441c4840913739db6edef0f6d21e
Max Roytman <imax531@gmail.com>
2016-12-11 18:36:25 +0200

    define mit added

--- compi2/max-rules.scm
+++ compi2/max-rules.scm
@@ -2,3 +2,9 @@
   (pattern-rule
    `(define ,(? 'var) ,(? 'val))
    (lambda (var val) `(def ,(parse var) ,(parse val)))))
+
+(define <define-mit-rule>
+  (pattern-rule
+   `(define (,(? 'f) . ,(? 'args)) ,(? 'body))
+   (lambda ((f . args) body)
+     `(def ,(parse f) ,(parse `(lambda ,args ,body))))))
