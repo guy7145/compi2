a4b2b73c75b304dd6f490e3364dfd7d32a9add5c
guy7145 <guy7145@gmail.com>
2016-12-12 21:55:14 +0200

    merged files with max

--- compi2/compiler.scm
+++ compi2/compiler.scm
@@ -919,6 +919,22 @@
    <and-rule-no-args>
    <and-rule-with-args>))
 
+#| .:: cond rule ::. ______________________________________________________________________________________________________________________________________________________|#
+
+(define <cond-rule>
+  (pattern-rule
+   `(cond ,(? 'expr) . ,(? 'exprs))
+   (lambda (head tail)
+     (letrec ((cond->if (lambda (lst)
+			  (if (null? lst)
+			      (void)
+			      (if (equal? 'else (caar lst))
+				  (beginify (cdar lst))
+				  `(if ,(caar lst)
+				       ,(beginify (cdar lst))
+				       ,(cond->if (cdr lst))))))))
+       (parse (cond->if `(,head ,@tail)))))))
+
 #| .:: define rules ::. ______________________________________________________________________________________________________________________________________________________|#
 
 (define <define-rule>
--- compi2/max-rules.scm
+++ compi2/max-rules.scm
@@ -1,34 +1,77 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(define <cond-rule>
+(define <let-no-args-rule>
   (pattern-rule
-   `(cond ,(? 'expr) . ,(? 'exprs))
-   (lambda (head tail)
-     (letrec ((cond->if (lambda (lst)
-                          (if (null? lst)
-                              (void)
-                              (if (equal? 'else (caar lst))
-                                  (beginify (cdar lst))
-                                  `(if ,(caar lst)
-                                       ,(beginify (cdar lst))
-                                       ,(cond->if (cdr lst))))))))
-       (parse (cond->if `(,head ,@tail)))))))
+   `(let () ,(? 'body) . ,(? 'rest))
+   (lambda (body . rest-body)
+     (let ((body (merge-bodies body rest-body)))
+       (parse `((lambda () ,body)))))))
 
+(define <let-rule>
+  (pattern-rule
+   `(let (,(? 'args) . ,(? 'rest)) ,(? 'body) . ,(? 'rest))
+   (lambda (args-head rest body . rest-body)
+     (let ((args (if (null? rest)
+		     `(,args-head)
+		     `(,args-head ,@rest)))
+	   (body (if (null? rest-body)
+		     `(,body)
+		     `(,body ,@(car rest-body)))))
+       (parse `((lambda ,(map car args) ,@body) ,@(map cadr args)))))))
 
+(define <let*-no-args-rule>
+  (pattern-rule
+   `(let* () ,(? 'body) . ,(? 'rest))
+   (lambda (body . rest-body)
+     (let ((body (merge-bodies body rest-body)))
+       (parse `((lambda () ,body)))))))
 
-                                        ;(display (format "\033[1;34m lst: ~s ;\033[0m " lst))(newline)
+(define <let*-rule>
+  (pattern-rule
+   `(let* (,(? 'args) . ,(? 'rest)) ,(? 'body) . ,(? 'rest))
+   (lambda (args-head rest head-body . rest-body)
+     (letrec ((args (if (null? rest)
+			`(,args-head)
+			`(,args-head ,@rest)))
+	      (body (if (null? rest-body)
+			`(,head-body)
+			`(,head-body ,@(car rest-body))))
+	      (let*->encapsulated-lambdas (lambda (args body)
+			       (if (null? args)
+				   body
+				   `(((lambda (,(caar args))
+					,@(let*->encapsulated-lambdas (cdr args) body))
+				      ,@(cdar args)))))))
+       (parse (car (let*->encapsulated-lambdas args body)))))))
+
+(define <letrec-no-args-rule>
+  (pattern-rule
+   `(letrec () ,(? 'body) . ,(? 'rest))
+   (lambda (body . rest-body)
+     (let ((body (merge-bodies body rest-body)))
+       (parse `((lambda () ((lambda () ,body)))))))))
+
+(define <letrec-rule>
+  (pattern-rule
+   `(letrec (,(? 'args) . ,(? 'rest)) ,(? 'body) . ,(? 'rest))
+   (lambda (args-head rest head-body . rest-body)
+     (letrec ((args (if (null? rest)
+			`(,args-head)
+			`(,args-head ,@rest)))
+	      (body (if (null? rest-body)
+			`(,head-body)
+			`(,head-body ,@(car rest-body))))
+	      (args->set (lambda (lst)
+			   (if (null? (cdr lst))
+			       `((set! ,(caar lst) ,@(cdar lst)))
+			       `((set! ,(caar lst) ,@(cdar lst)) ,@(args->set (cdr lst)))))))
+       (display (append (args->set args) body)) (newline)
+       (parse `((lambda ,(map car args) ,@(append (args->set args) `(((lambda () ,@body))))) ,@(map (lambda (x) #f) args)))))))
+
+(define <let-rules>
+  (compose-patterns
+   <let-no-args-rule>
+   <let-rule>
+   <let*-no-args-rule>
+   <let*-rule>
+   <letrec-no-args-rule>
+   <letrec-rule>
+   ))
